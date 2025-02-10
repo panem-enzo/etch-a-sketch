0d535f1e7609142708cf47528a8669d84b48bf9b
Enzo Panem <panemjf@gmail.com>
2025-02-10 18:23:17 -0500

    Completed implentation of the grid size input

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-const grid = document.querySelector(".grid");
-
-let numRows = 16;
-let numCols = 16;
-
-for (r = 0; r < numRows; r++) {
-  const row = document.createElement("div");
-  row.setAttribute("style", "display: flex;");
-  grid.appendChild(row);
-
-  for (c = 0; c < numCols; c++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.setAttribute(
-        "style",
-        "height: 25px; width: 25px; border: solid; border-width: 1px;"
-      );
-    gridSquare.addEventListener("mouseover", () => {
-      gridSquare.style.backgroundColor = "black";
-      gridSquare.style.transition = "background-color ease 0.2s";
-    });
-    
-    row.appendChild(gridSquare);
-  }
+function promptUser() {
+  const startBtn = document.querySelector("button");
+  startBtn.addEventListener("click", () => {
+    let size = prompt("What is the size 'N', of your 'NxN' grid?");
+    generateGrid(size, size);
+  }); 
 }
+
+function generateGrid(numRows, numCols) {
+
+  const grid = document.querySelector(".grid");
+
+  for (r = 0; r < numRows; r++) {
+    const row = document.createElement("div");
+    row.setAttribute("style", "display: flex;");
+    grid.appendChild(row);
+  
+    for (c = 0; c < numCols; c++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.setAttribute(
+          "style",
+          "height: 25px; width: 25px; border: solid; border-width: 1px;"
+        );
+      gridSquare.addEventListener("mouseover", () => {
+        gridSquare.style.backgroundColor = "black";
+        gridSquare.style.transition = "background-color ease 0.2s";
+      });
+      
+      row.appendChild(gridSquare);
+    }
+  } 
+}
+
+promptUser();
