3c89fd3888b6a3974f71935cf036d8c0c340993b
panem-enzo <epanem@my.yorku.ca>
2025-02-10 23:20:44 -0500

    Completed implementation of grid removal after clicking create grid button

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-const grid = document.querySelector(".grid");
+const container = document.querySelector(".container");
+container.style = "width: 960px;";
 
-let numRows = 16;
-let numCols = 16;
+function createGridNode(size) {
+  const grid = document.createElement("div");
+  grid.className = "grid";
+  container.appendChild(grid);
+  generateGrid(grid, size, size);
+}
 
-for (r = 0; r < numRows; r++) {
-  const row = document.createElement("div");
-  row.setAttribute("style", "display: flex;");
-  grid.appendChild(row);
-  for (c = 0; c < numCols; c++) {
-    const column = document.createElement("div");
-    column.setAttribute(
+function promptUser() {
+  const startBtn = document.querySelector("button");
+
+  startBtn.addEventListener("click", () => {
+    let size = prompt("What is the size 'N', of your 'NxN' grid?");
+    if (document.querySelector(".grid")) document.querySelector(".grid").remove();
+    createGridNode(size);
+  });
+}
+
+function generateGrid(grid, numRows, numCols) {
+
+  for (r = 0; r < numRows; r++) {
+    const row = document.createElement("div");
+    row.setAttribute("style", "display: flex;");
+    grid.appendChild(row);
+
+    for (c = 0; c < numCols; c++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.setAttribute(
         "style",
         "height: 25px; width: 25px; border: solid; border-width: 1px;"
       );
-    row.appendChild(column);
+      gridSquare.addEventListener("mouseover", () => {
+        gridSquare.style.backgroundColor = "black";
+        gridSquare.style.transition = "background-color ease 0.2s";
+      });
+
+      row.appendChild(gridSquare);
+    }
   }
 }
+
+promptUser();
